a12f73e67b06c05b6fff4e0d513e1b6b0a3d7b77
zehrakaragul <107124243+zehrakaragul@users.noreply.github.com>
2023-06-16 14:04:11 +0300

    medicine reminder app with Flutter

---
+++ untitled/android/settings.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/profil.dart';
+
+
+class AyarPage extends StatefulWidget {
+  const AyarPage({Key? key}) : super(key: key);
+
+  @override
+  State<AyarPage> createState() => _AyarPageState();
+}
+
+class _AyarPageState extends State<AyarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+    );
+  }
+}
